000341de3a39f9c1be12ca83f1ddd962c53e0166
lpq <lpq>
2019-12-07 17:41:01 +0800

    改变第六次

--- src/main/java/com/asd.java
+++ src/main/java/com/asd.java
@@ -2,7 +2,7 @@ package com;
 
 public class asd {
     public static void main(String[] args) {
-        System.out.println("改变第五次");
-        System.out.println("改变第四次");
+        System.out.println("改变第六次");
+
     }
 }
